dea2093e7892ad2e4f0464f20b1260e0dea7a04a
serraguti <pacoserranox@gmail.com>
2024-10-24 14:16:15 +0200

    aaaa

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HolaMundo/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HolaMundo from './components/HolaMundo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, HolaMundo
   }
 }
 </script>
---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Mi primera App de Vue, que ilusion!!!</h1>
+    </div>
+</template>
+
+<script>
+    //ESTO ES CODIGO JS (ESPECIAL)
+    //CADA COMPONENT QUE CREEMOS SIEMPRE DEBEMOS EXPORTARLO
+    //CON UN name
+    export default {
+        name: "HolaMundo"
+    }
+</script>
+
+<style>
+    h1 {color:fuchsia}
+</style>
